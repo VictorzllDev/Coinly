b0669643637bafa7766a2b000c54ea561cc585cf
VictorzllDev <VictorzllDev@gmail.com>
2025-07-03 15:18:10 -0300

    settings initial. add tailwindcss, shadciu, vite, typescript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+export function App() {
+	return (
+		<>
+			<p className="text-3xl text-green-500">Welcome to Coinly</p>
+			<Button>click aqui</Button>
+		</>
+	)
+}
